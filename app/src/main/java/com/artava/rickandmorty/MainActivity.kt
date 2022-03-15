20626be6b76867906571e487f6d3349f56159f21
Alekseevvv <alekseev.vladimir1212@gmail.com>
2022-03-16 00:19:38 +0300

    add menu

--- app/src/main/java/com/artava/rickandmorty/MainActivity.kt
+++ app/src/main/java/com/artava/rickandmorty/MainActivity.kt
@@ -1,13 +1,45 @@
 package com.artava.rickandmorty
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        supportFragmentManager.beginTransaction().add(R.id.rick_fragment, CharacterFragment())
+            .commit()
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).setOnItemSelectedListener {
+            item ->
+            when(item.itemId){
+                R.id.page_1 -> {
+                    // Respond to navigation item 1 click
+                    println("Str1")
+                    supportFragmentManager.beginTransaction().replace(R.id.rick_fragment, CharacterFragment())
+                        .commit()
+                    true
+                }
+                R.id.page_2 -> {
+                    // Respond to navigation item 2 click
+                    println("Str2")
+                    supportFragmentManager.beginTransaction().replace(R.id.rick_fragment, LocationsFragment())
+                        .commit()
 
+                    true
+                }
+                R.id.page_3 -> {
+                    // Respond to navigation item 2 click
+                    println("Str3")
+                    supportFragmentManager.beginTransaction().replace(R.id.rick_fragment, EpisodesFragment())
+                        .commit()
+
+                    true
+                }
+                else -> false
+            }
+
+        }
     }
 }
